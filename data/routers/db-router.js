a7c9d7af9a3350759d3bb9bf8dc1b615970c3cad
mxxt1 <matt@dustcloudlabs.com>
2019-11-04 15:37:03 -0500

    MVP queries

---
+++ data/routers/db-router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const db = require('../dbConfig.js');
+
+const router = express.Router();
+
+//router.get()
+
+
+//router.post()
+
+
+//router.put()
+
+
+//router.delete()
